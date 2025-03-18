88ba5648cc72ca564a3f33955663dc340eed6727
ylmzunal <yilmazunal16@gmail.com>
2025-03-18 20:20:29 +0300

    bugs fixed

--- src/main/java/dev/swe573/whatsthis/WhatsthisApplication.java
+++ src/main/java/dev/swe573/whatsthis/WhatsthisApplication.java
@@ -3,12 +3,24 @@ package dev.swe573.whatsthis;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+import java.io.File;
 
 @SpringBootApplication
-@PropertySource("file:env.properties")
+@PropertySources({
+    @PropertySource(value = "file:env.properties", ignoreResourceNotFound = true)
+})
 public class WhatsthisApplication {
 
 	public static void main(String[] args) {
+		// Check if env.properties file exists
+		File envFile = new File("env.properties");
+		if (envFile.exists()) {
+			System.out.println("env.properties file found at: " + envFile.getAbsolutePath());
+		} else {
+			System.out.println("env.properties file not found. Using environment variables only.");
+		}
+		
 		SpringApplication.run(WhatsthisApplication.class, args);
 	}
 
